582cd79097ba98f8a23734b4188630118f5f1364
张浩斌 <newsclan@gmail.com>
2019-06-07 19:12:23 +0800

    Update mail.py

--- mail.py
+++ mail.py
@@ -84,7 +84,7 @@ while index >0 :
         server.dele(index)
         # 关闭连接:
         index=index-1
-        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36'}
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3202.89 Safari/537.36'}
         cook = {"Cookie":'BAIDUID=FE0F97F1FC37C47792091A2523CD945F:FG=1; HMACCOUNT=CC6D0E280C842123'}
         res=requests.get(url,cookies=cook,headers=headers)
         res.encoding='utf-8'
