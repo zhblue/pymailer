215408f65e7bca3a46de974935fbe6192ca6a937
张浩斌 <newsclan@gmail.com>
2021-11-23 17:19:16 +0800

    Update executer.py

--- executer.py
+++ executer.py
@@ -140,12 +140,12 @@ while index >0 :
         # 可以根据邮件索引号直接从服务器删除邮件:
         print("delete "+str(index))
         server.dele(index)
-        # 关闭连接:
+        index=index-1
+        # 关闭连接
         if cmd.lower()=="cmd" :
                 tp=""
                 mail_msg="<pre>"+execute_cmd(get_content(msg))+"<pre>"
         else:
-                index=index-1
                 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3202.89 Safari/537.36'}
                 cook = {"Cookie":'BAIDUID=FE0F97F1FC37C47792091A2523CD945F:FG=1; HMACCOUNT=CC6D0E280C842123'}
                 lit = ['caj','htm','html','pdf', 'zip', 'rar', 'gz','exe','msi','xls','doc','xlsx','docx','txt','jpg','jpeg','png','gif','mp4','mp3','mid']
